62fa95b0901ade33250bbe26ad67ee66fcae5597
Curtis.Lovrak <curtislovrak@gmail.com>
2025-01-07 15:50:57 -0500

    moved env variables

--- server.ts
+++ server.ts
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-export const port = process.env.PORT || 5000;
+export const port = process.env.PORT;
 const uri = process.env.MONGODB_URI!;
 
 const client = new MongoClient(uri, {
