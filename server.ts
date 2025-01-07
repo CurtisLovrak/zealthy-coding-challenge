8e8bdace65f66c0c43968ea15ea9b5e6c9f2960c
Curtis.Lovrak <curtislovrak@gmail.com>
2025-01-07 13:19:02 -0500

    added port fallback

--- server.ts
+++ server.ts
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-export const port = process.env.PORT
+export const port = process.env.PORT || 5000;
 const uri = process.env.MONGODB_URI!;
 
 const client = new MongoClient(uri, {
